c39c8452828c1d1a2967a0f9cf68458b05664ab6
Laurence-USC <olivier_bbs@outlook.com>
2015-03-02 22:24:23 -0800

    Source files deleted for ilegal names.

--- edu/usc/util/DataExtractor.java
+++
@@ -1,9 +0,0 @@
-package edu.usc.util;
-
-import java.util.ArrayList;
-
-public abstract class DataExtractor implements java.io.Serializable {
-    public DataExtractor(){}
-
-    public abstract ArrayList<DataRecord> extractData();
-}
