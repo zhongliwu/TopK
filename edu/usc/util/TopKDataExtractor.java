925310a22bdca83fbbc4f00aac1c116c234d3b07
Laurence-USC <olivier_bbs@outlook.com>
2015-03-08 15:05:16 -0700

    1. Delete Maven Packages.

--- edu/usc/util/TopKDataExtractor.java
+++
@@ -1,9 +0,0 @@
-package edu.usc.util;
-
-import java.util.ArrayList;
-
-public abstract class TopKDataExtractor implements java.io.Serializable {
-    public TopKDataExtractor(){}
-
-    public abstract ArrayList<TopKDataRecord> extractData();
-}
